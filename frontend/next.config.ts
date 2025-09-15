204638ea9d608b1db1a0155ba00cca3ce707fc87
Khushal <khushal2004a@gmail.com>
2025-09-15 13:52:26 +0530

    fix: modules

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,36 +1,12 @@
-const withTM = require("next-transpile-modules")([
-  "@blocto/sdk",
-  "@project-serum/sol-wallet-adapter",
-  "@solana/wallet-adapter-base",
-  "@solana/wallet-adapter-react",
-  "@solana/wallet-adapter-bitkeep",
-  "@solana/wallet-adapter-bitpie",
-  "@solana/wallet-adapter-blocto",
-  "@solana/wallet-adapter-clover",
-  "@solana/wallet-adapter-coin98",
-  "@solana/wallet-adapter-coinhub",
-  "@solana/wallet-adapter-ledger",
-  "@solana/wallet-adapter-mathwallet",
-  "@solana/wallet-adapter-phantom",
-  "@solana/wallet-adapter-safepal",
-  "@solana/wallet-adapter-slope",
-  "@solana/wallet-adapter-solflare",
-  "@solana/wallet-adapter-sollet",
-  "@solana/wallet-adapter-solong",
-  "@solana/wallet-adapter-tokenpocket",
-  "@solana/wallet-adapter-torus",
-  "@solana/wallet-adapter-wallets",
-]);
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = withTM({
-  reactStrictMode: true,
-  webpack5: true,
+const nextConfig: NextConfig = {
+  /* config options here */
   experimental: {
     serverActions: {
-      bodySizeLimit: "6mb",
+      bodySizeLimit: '6mb', // increase as needed
     },
   },
-});
+};
 
-module.exports = nextConfig;
+export default nextConfig;
